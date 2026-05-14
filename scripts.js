d15659a28f6676baed12f22e31373c5dd31c201d
João <joaovitorresende.jr@gmail.com>
2026-05-14 17:41:18 -0300

    feat: criando elemento imagem, div, strong e span e adicionando seus conteudos de forma dinamica

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const expense = document.getElementById("expense")
 const category = document.getElementById("category")
+const expenseList = document.querySelector("ul")
+
 amount.oninput = () => {
     let value = amount.value.replace(/\D+/g,"")
     value = Number(value)/100
@@ -24,6 +26,23 @@ function expenseAdd(newExpense){
     try {
         const expenseItem = document.createElement("li")
         expenseItem.classList.add("expense")
+
+        const expenseIcon = document.createElement("img")
+        expenseIcon.setAttribute("src",`img/${newExpense.category_id}.svg`)
+        expenseIcon.setAttribute("alt", newExpense.category_name)
+        expenseItem.append(expenseIcon)
+        expenseList.append(expenseItem)
+
+        const expenseInfo = document.createElement("div")
+        expenseInfo.classList.add("expense-info");
+        const expenseName = document.createElement("strong")
+        expenseName.textContent = newExpense.expense
+        const expenseCategory = document.createElement("span")
+        expenseCategory.textContent = newExpense.category_name
+        expenseInfo.append(expenseName, expenseCategory)
+        expenseItem.append(expenseInfo)
+        expenseList.append(expenseItem)
+
     } catch (error) {
         alert("Não foi possivel cadastrar a dispesa")
         console.log(error)

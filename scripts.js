ec9ae0ce942e87d4ea66fc3a1197637d3001d02f
João <joaovitorresende.jr@gmail.com>
2026-05-16 16:59:08 -0300

    chore: adicionando total formatado

--- scripts.js
+++ scripts.js
@@ -1,90 +1,103 @@
-const form = document.querySelector("form")
-const amount = document.getElementById("amount")
-const expense = document.getElementById("expense")
-const category = document.getElementById("category")
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
 
-const expenseList = document.querySelector("ul")
-const expenseQuantity = document.querySelector("aside header p span")
-
+const expenseList = document.querySelector("ul");
+const expenseQuantity = document.querySelector("aside header p span");
+const expenseTotal = document.querySelector("aside header h2")
 amount.oninput = () => {
-    let value = amount.value.replace(/\D+/g,"")
-    value = Number(value)/100
-    amount.value = formatCurrencyBRL(value)
+  let value = amount.value.replace(/\D+/g, "");
+  value = Number(value) / 100;
+  amount.value = formatCurrencyBRL(value);
+};
+form.onsubmit = (event) => {
+  event.preventDefault();
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  };
+  expenseAdd(newExpense);
+};
+
+function expenseAdd(newExpense) {
+  try {
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
+
+    const expenseIcon = document.createElement("img");
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+    expenseIcon.setAttribute("alt", newExpense.category_name);
+    expenseItem.append(expenseIcon);
+    expenseList.append(expenseItem);
+
+    const expenseInfo = document.createElement("div");
+    expenseInfo.classList.add("expense-info");
+    const expenseName = document.createElement("strong");
+    expenseName.textContent = newExpense.expense;
+    const expenseCategory = document.createElement("span");
+    expenseCategory.textContent = newExpense.category_name;
+    expenseInfo.append(expenseName, expenseCategory);
+    expenseItem.append(expenseInfo);
+    expenseList.append(expenseItem);
+
+    const expenseAmount = document.createElement("span");
+    expenseAmount.classList.add("expense-amount");
+    expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}`;
+    expenseItem.append(expenseAmount);
+    expenseList.append(expenseItem);
+
+    const removeIcon = document.createElement("img");
+    removeIcon.classList.add("remove-icon");
+    removeIcon.setAttribute("src", "img/remove.svg");
+    removeIcon.setAttribute("alt", "remover");
+    expenseItem.append(removeIcon);
+    expenseList.append(expenseItem);
+
+    updateTotals();
+  } catch (error) {
+    alert("Não foi possivel cadastrar a dispesa");
+    console.log(error);
+  }
 }
-form.onsubmit = (event) =>{
-    event.preventDefault()
-    const newExpense ={
-        id: new Date().getTime(),
-        expense: expense.value,
-        category_id: category.value,
-        category_name: category.options[category.selectedIndex].text,
-        amount: amount.value,
-        created_at: new Date(),
+
+function formatCurrencyBRL(value) {
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+  return value;
+}
+
+function updateTotals() {
+  try {
+    let total = 0;
+    const items = expenseList.children;
+    expenseQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`;
+
+    for (let item = 0; item < items.length; item++) {
+      const itemAmount =
+        items[item].querySelector(".expense-amount").textContent;
+
+      const value = itemAmount
+        .replace("R$", "")
+        .replace(/\./g, "")
+        .replace(",", ".")
+        .trim();
+      total += Number(value);
     }
-    expenseAdd(newExpense)
-}
 
-function expenseAdd(newExpense){
-    try {
-        const expenseItem = document.createElement("li")
-        expenseItem.classList.add("expense")
-
-        const expenseIcon = document.createElement("img")
-        expenseIcon.setAttribute("src",`img/${newExpense.category_id}.svg`)
-        expenseIcon.setAttribute("alt", newExpense.category_name)
-        expenseItem.append(expenseIcon)
-        expenseList.append(expenseItem)
-
-        const expenseInfo = document.createElement("div")
-        expenseInfo.classList.add("expense-info");
-        const expenseName = document.createElement("strong")
-        expenseName.textContent = newExpense.expense
-        const expenseCategory = document.createElement("span")
-        expenseCategory.textContent = newExpense.category_name
-        expenseInfo.append(expenseName, expenseCategory)
-        expenseItem.append(expenseInfo)
-        expenseList.append(expenseItem)
-
-        const expenseAmount = document.createElement("span")
-        expenseAmount.classList.add("expense-amount")
-        expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$","")}`
-        expenseItem.append(expenseAmount)
-        expenseList.append(expenseItem)
-
-        const removeIcon = document.createElement("img")
-        removeIcon.classList.add("remove-icon")
-        removeIcon.setAttribute("src","img/remove.svg")
-        removeIcon.setAttribute("alt","remover")
-        expenseItem.append(removeIcon)
-        expenseList.append(expenseItem)
-
-        updateTotals()
-    } catch (error) {
-        alert("Não foi possivel cadastrar a dispesa")
-        console.log(error)
-    }
-}
-
-function formatCurrencyBRL(value){
-    value = value.toLocaleString("pt-BR",{
-        style: "currency",
-        currency: "BRL",
-    })
-    return value
-}
-
-function updateTotals(){
-    try {
-        let total = 0
-        const items = expenseList.children
-        expenseQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
-        for (let item = 0; item < item.length; i++)
-        {
-            const itemAmount = items[item].querySelector(".expense-amount")
-        }
-
-    } catch (error) {
-        console.log(error)
-        alert("Não foi possivel atualizar totais")
-    }
+    const symbolBRL = document.createElement("small")
+    symbolBRL.textContent = "R$"
+    total = formatCurrencyBRL(total).replace("R$", "").trim()
+    expenseTotal.innerHTML = "" //limpando o conteudo e embaixo adicionando novamente
+    expenseTotal.append(symbolBRL, total)
+  } catch (error) {
+    console.log(error);
+    alert("Não foi possivel atualizar totais");
+  }
 }
